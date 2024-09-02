018ec20f6ba3eb79d651fb11a4f53ce9316c45a9
Yu <u7670957@anu.edu.au>
2024-09-02 20:06:02 +1000

    delete files

--- sketch.js
+++ sketch.js
@@ -155,13 +155,39 @@ function mouseDragged() {
   steeringWheel.drag(mouseX, mouseY);  // 拖动方向盘
 }
 
-// function keyPressed() {
-//   steeringWheel.keyControl(keyCode);  // 键盘控制方向盘
-// }
 
 // 当按下空格键时，将当前画布内容保存为"thumbnail.png"文件
 function keyTyped() {
   if (key === " ") {
     saveCanvas("thumbnail.png");
+  }
+}
+
+class Tree {
+  constructor(x, y, trunkWidth, trunkHeight, leafSize) {
+    this.x = x;
+    this.y = y;
+    this.trunkWidth = trunkWidth;
+    this.trunkHeight = trunkHeight;
+    this.leafSize = leafSize;
+  }
+
+  drawTrunk() {
+    fill(97, 62, 33);
+    noStroke();
+    rect(this.x - this.trunkWidth / 2, this.y - this.trunkHeight / 2, this.trunkWidth, this.trunkHeight);
+    rect(this.x - this.trunkWidth / 2, this.y - this.trunkHeight * 1.5, this.trunkWidth, this.trunkHeight);
+  }
+
+  drawLeaves() {
+    fill(40, 87, 39);
+    circle(this.x, this.y - this.trunkHeight * 2, this.leafSize);
+    circle(this.x + this.trunkWidth, this.y - this.trunkHeight, this.leafSize);
+    circle(this.x - this.trunkWidth, this.y - this.trunkHeight, this.leafSize);
+  }
+
+  draw() {
+    this.drawTrunk();
+    this.drawLeaves();
   }
 }
